4d3da918778eabdf01fd0fbebefcb04edc4aca32
Frieder Bluemle <frieder.bluemle@gmail.com>
2017-05-12 00:55:33 +0800

    Extend AppCompatTextView
    This fixes a Lint error.

--- app/src/main/java/com/scottyab/rootbeer/sample/TextViewFont.java
+++ app/src/main/java/com/scottyab/rootbeer/sample/TextViewFont.java
@@ -3,14 +3,14 @@ package com.scottyab.rootbeer.sample;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.widget.TextView;
 
 /**
  * Created by 1andbarb on 22/04/2016.
  */
-public class TextViewFont extends TextView {
+public class TextViewFont extends AppCompatTextView {
 
     private static final String TAG = "TextViewFont";
 
