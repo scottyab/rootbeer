8896fce601f55eee605488a24dfe6f800acf1309
Kwasi Clouden <clouden@unlv.nevada.edu>
2026-02-02 22:31:26 -0800

    Add repository mining scripts for assignment

--- Kwasi_CollectFiles.py
+++ Kwasi_CollectFiles.py
@@ -68,7 +68,7 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise they will all be reverted and you will have to re-create them
 # I would advise to create more than one token for repos with heavy commits
-lstTokens = ["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"]
+lstTokens = ["ghp_zYzg2WzooqcCfhghofnjdfjfsr"]
 
 dictfiles = dict()
 countfiles(dictfiles, lstTokens, repo)
